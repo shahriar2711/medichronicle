7acc15080d3f9aa3e53eda66f3ca38f80e541523
shahriar2711 <atiqshahriar2001@gmail.com>
2024-05-18 11:09:57 +0600

    Create Prescription.jsx

---
+++ src/components/pages/Prescription.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+
+function Prescription() {
+  
+  const [file , setFile] = useState()
+
+  function handleFile(event){
+    setFile(event.target.files[0])
+    
+  }
+
+  function handleUpload() {
+    const formData = new FormData()
+    formData.append('file' , file)
+    fetch(
+      'url',
+      {
+        method:"POST",
+        body:formData
+      }
+    ).then((response) => response.json())
+    .then(
+      (result) => {
+        console.log('success',result)
+      }
+    )
+    .catch(error => {
+      console.error("Error:" , error)
+    })
+  }
+
+  return (
+    <>
+    <div>
+      <h2>Upload your prescription here</h2>
+      <form>
+        <input type="file" name='file' onChange={handleFile} />
+        <button>Upload</button>
+      </form>
+    </div>
+    </>
+  )
+}
+
+export default Prescription;
